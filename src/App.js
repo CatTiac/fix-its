554c06f82d574536e0c90e172e5a743cf65a831b
Cait J <95374937+CatTiac@users.noreply.github.com>
2022-02-15 16:24:08 +0000

    first fix-it complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import FixIt1 from "./Fix-it/Fix-it-1";
+import FixIt2 from "./Fix-it/Fix-it-2";
 import Fixit3 from "./Fix-it/Fix-it-3";
 
 const App = () => {
   return (
     <div>
-      <div>
-        <FixIt1 />
-        <FixIt2 />
-        <Fixit3 />
-      </div>
+      <FixIt1 />
+      <FixIt2 />
+      <Fixit3 />
     </div>
   );
 }
--- src/Fix-it/Fix-it-3.js
+++ src/Fix-it/Fix-it-3.js
@@ -1,13 +1,14 @@
 const Fixit3 = () => {
-    return (
-        <h1>Fix it 3</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Vulputate ut
-          pharetra sit amet aliquam id diam.
-        </p>
-      )
-  };
-  
-  export default Fixit3;
-  
+  return (
+    <div>
+      <h1>Fix it 3</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Vulputate ut
+        pharetra sit amet aliquam id diam.
+      </p>
+    </div>
+  )
+};
+
+export default Fixit3;
